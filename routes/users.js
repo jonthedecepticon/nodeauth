ef8d82b3b97b9f2ab9c610de4f91e1eb8769ecd0
Jon Lambson <jon.lambson@daplie.com>
2017-10-09 21:38:56 -0600

    created more views

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
+router.get('/register', (req, res, next) => {
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Login' });
+});
+
 module.exports = router;
