80fe03ed3171e550ebf036016f8a4858da599b2d
Jon Lambson <jon.lambson@daplie.com>
2017-11-21 00:11:16 -0700

    updated npm and fixed mongo error

--- models/user.js
+++ models/user.js
@@ -1,30 +1,33 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/nodeauth');
+mongoose.connect('mongodb://localhost/nodeauth', {
+  useMongoClient: true,
+});
 
 var db = mongoose.connection;
 
-var UserSchema = mongoose.schema({
-  username: {
-    type: String,
-    index: true
-  },
-  password: {
-    type: String
-  },
-  email: {
-    type: String
-  },
-  name: {
-    type: String
-  },
-  profileimage: {
-    type: String
-  }
+// User Schema
+var UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		index: true
+	},
+	password: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	name: {
+		type: String
+	},
+	profileimage:{
+		type: String
+	}
 });
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.createUser = function (newUser, callback) {
-  newUser.save(callback);
-};
+module.exports.createUser = function(newUser, callback){
+	newUser.save(callback);
+}
